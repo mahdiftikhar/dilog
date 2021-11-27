1cd1cc5d39e23558db5a64c7075a85ce76a50a3e
Antonnio_Toni <blooddreamer70@gmail.com>
2021-11-28 00:48:50 +0500

    Profile Page

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const HOST = "localhost";
 const USER = "root";
 const DATABASE = "dilog";
-const PASSWORD = "mightywings42";
+const PASSWORD = "cumdump";
 
 const pool = mysql.createPool({
     host: HOST,
