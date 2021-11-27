c99d988baeaf4cf95f7a69937abee8783999d7e0
Antonnio_Toni <blooddreamer70@gmail.com>
2021-11-27 19:11:18 +0500

    commit before changes

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const flash = require("connect-flash");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
-
 const db = require("./util/database");
 
 const app = express();
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(
     session({ secret: "my secret", resave: false, saveUninitialized: false })
 );
-
+app.use(flash());
 // app.use((req, res, next) => {
 //     User.findById()
 //         .then((user) => {
@@ -38,7 +38,8 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-const port = 3029;
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+const port = 3000;
+app.listen(port, (err) => {
+    if (err) console.log(err);
+    // console.log(`Server listening on port ${port}`);
 });
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,61 @@
+const bcrypt = require("bcryptjs");
+
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
+    let message = req.flash("error");
+
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
+
     res.render("user/login", {
         pageTitle: "Log in",
         path: "/",
+        errorMessage: message,
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    User.findByName("nullptr")
+    const userName = req.body.userName;
+    const password = req.body.password;
+
+    User.findByName(userName)
         .then(([data, metadata]) => {
             const user = data[0];
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save((err) => {
-                console.log(err);
-                res.redirect("/home");
-            });
+
+            if (!user) {
+                req.flash("error", "Invalid email or password");
+                return res.redirect("/");
+            }
+
+            bcrypt
+                .compare(password, user.password)
+                .then((doMatch) => {
+                    if (!doMatch) {
+                        return res.redirect("/");
+                    }
+
+                    req.session.isLoggedIn = true;
+                    req.session.user = user;
+                    req.session.save((err) => {
+                        if (err) console.log(err);
+                        res.redirect("/home");
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.redirect("/");
+                });
         })
         .catch((err) => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy((err) => {
-        console.log(err);
+        if (err) console.log(err);
         res.redirect("/");
     });
 };
@@ -34,4 +65,43 @@ exports.getSignup = (req, res, next) => {
         pageTitle: "Signup",
         path: "/signup",
     });
+};
+
+exports.postSignup = (req, res, next) => {
+    const userName = req.body.userName;
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    const dateOfBirth = req.body.dateOfBirth;
+
+    if (password !== confirmPassword) {
+        return res.redirect("/signup");
+    }
+
+    User.findByName(userName)
+        .then(([data, metaData]) => {
+            if (data[0]) {
+                return res.redirect("/signup");
+            }
+            return bcrypt
+                .hash(password, 12)
+                .then((hashedPassword) => {
+                    const user = new User(
+                        userName,
+                        email,
+                        null,
+                        dateOfBirth,
+                        null,
+                        hashedPassword
+                    );
+                    return user.save();
+                })
+                .then((result) => {
+                    return res.redirect("/");
+                });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.redirect("/signup");
+        });
 };
