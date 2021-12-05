808e40dce152c53d8c4e7cba0ce318af2852c839
ShayaanJ <23100208@lums.edu.pk>
2021-12-05 19:49:19 +0500

    Databse updated

--- dilog-database.sql
+++ dilog-database.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `dilog` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `dilog`;
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
 -- Host: localhost    Database: dilog
@@ -40,6 +38,7 @@ CREATE TABLE `admin` (
 
 LOCK TABLES `admin` WRITE;
 /*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES ('admin','admin@gmail.com','$2a$12$8de9CVtL4sfuaUG66S5Lq.9EHt3OlLpsFuA91U0PCnw.Ypcqifj4W');
 /*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -209,7 +208,7 @@ CREATE TABLE `post` (
 
 LOCK TABLES `post` WRITE;
 /*!40000 ALTER TABLE `post` DISABLE KEYS */;
-INSERT INTO `post` VALUES (1,'umair14040','firstpost happy','Hello this is my first post',_binary '0',0,'2021-11-16 09:44:00'),(3,'sjc','firstpost happy','Hello this is my first post',_binary '0',0,'2021-11-16 09:44:00'),(4,'antonnio','firstpost happy','lorem ipsum',_binary '0',0,'2021-11-16 09:44:00'),(5,'nullptr','firstpost happy','mujhay maar do',_binary '0',0,'2021-11-16 09:44:00'),(6,'nullptr','firstpost happy','mujhay maar do please',_binary '0',0,'2021-11-16 09:44:00'),(7,'umair14040','lol','what is this? i am tired. DB sucks',_binary '0',0,'2021-11-18 09:11:00'),(8,'nullptr','music','jaldee karo mujhay susu aa raha hay. khalee glass me doodh kyun para hay????',NULL,1,'2021-11-18 09:11:15'),(9,'sjc','music','menay masqurade ball pe jaana tha yaar. me db kyun kar raha hoon',NULL,0,'2021-11-18 09:11:16');
+INSERT INTO `post` VALUES (1,'umair14040','firstpost happy','Hello this is my first post',_binary '0',0,'2021-11-16 09:44:00'),(3,'sjc','firstpost happy','Hello this is my first post',_binary '0',0,'2021-11-16 09:44:00'),(4,'antonnio','firstpost happy','lorem ipsum',_binary '0',0,'2021-11-16 09:44:00'),(5,'nullptr','firstpost happy','mujhay maar do',_binary '0',0,'2021-11-16 09:44:00'),(6,'nullptr','firstpost happy','mujhay maar do please',_binary '0',0,'2021-11-16 09:44:00'),(7,'umair14040','lol','what is this? i am tired. DB sucks',_binary '0',0,'2021-11-18 09:11:00'),(8,'nullptr','music','jaldee karo mujhay susu aa raha hay. khalee glass me doodh kyun para hay????',NULL,1,'2021-11-18 09:11:15'),(9,'sjc','music','menay masqurade ball pe jaana tha yaar. me db kyun kar raha hoon',NULL,2,'2021-11-18 09:11:16');
 /*!40000 ALTER TABLE `post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -262,7 +261,7 @@ CREATE TABLE `postreacts` (
 
 LOCK TABLES `postreacts` WRITE;
 /*!40000 ALTER TABLE `postreacts` DISABLE KEYS */;
-INSERT INTO `postreacts` VALUES (8,'user');
+INSERT INTO `postreacts` VALUES (9,'test'),(9,'test_new'),(8,'user');
 /*!40000 ALTER TABLE `postreacts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -310,7 +309,7 @@ CREATE TABLE `reportposts` (
   UNIQUE KEY `reportId_UNIQUE` (`reportId`),
   KEY `postid3_idx` (`postId`),
   CONSTRAINT `postid3` FOREIGN KEY (`postId`) REFERENCES `post` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -319,7 +318,7 @@ CREATE TABLE `reportposts` (
 
 LOCK TABLES `reportposts` WRITE;
 /*!40000 ALTER TABLE `reportposts` DISABLE KEYS */;
-INSERT INTO `reportposts` VALUES (1,9,'2021-11-29 23:48:03','spam');
+INSERT INTO `reportposts` VALUES (1,9,'2021-11-29 23:48:03','spam'),(2,9,'2021-12-04 02:27:24','spam');
 /*!40000 ALTER TABLE `reportposts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -377,17 +376,9 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('antonnio','antonnio@gmail.com',_binary '0','1999-06-19','my name is afaq','password12345'),('beanbunny','beanbunny@gmail.com',_binary '0','2001-09-20','my name is ahmad','password12345'),('newUser','newUser@gmail.com',NULL,'2021-11-16',NULL,'test'),('nullptr','nullptr@gmail.com',_binary '0','1999-10-19','my name is mahd','password12345'),('sjc','jamilshayaan@gmail.com',_binary '0','2000-10-09','my name is shayaan','password12345'),('umair14040','umair2000yousaf@gmail.com',_binary '0','2000-12-30','my name is umair','password123'),('user','user@gmai.com',NULL,'2021-11-02','Hello. I am a test user. I like to suck cock!','$2a$12$SF/5hxCP//4BLg7oWzywwubl1NA1oRXEHRVPEDbkuGdvpCNBJb7Eq'),('user2','uasdf.afasd@fgmad.com',NULL,'2021-11-04',NULL,'$2a$12$XqEXCYqd2kGnoWW122swBOKGOeX3kwy6Sd8ki2r1Ig5CFGVyItrLO');
+INSERT INTO `user` VALUES ('antonnio','antonnio@gmail.com',_binary '0','1999-06-19','my name is afaq','password12345'),('beanbunny','beanbunny@gmail.com',_binary '0','2001-09-20','my name is ahmad','password12345'),('newUser','newUser@gmail.com',NULL,'2021-11-16',NULL,'test'),('nullptr','nullptr@gmail.com',_binary '0','1999-10-19','my name is mahd','password12345'),('sjc','jamilshayaan@gmail.com',_binary '0','2000-10-09','my name is shayaan','password12345'),('test','test@gmail.com',NULL,'2021-12-25',NULL,'$2a$12$8oFiJYNtxGi17UeRMd7Pv.gliBkP/4iJL8Q2Vrfg68yI2u1xGMB26'),('test_new','new@gmail.com',NULL,'2021-11-30',NULL,'$2a$12$JpVVnQjjZh9LoiUiJqhEp.NUPydOQUxXxYptYnM4VlS2rNS8aqGie'),('umair14040','umair2000yousaf@gmail.com',_binary '0','2000-12-30','my name is umair','password123'),('user','user@gmai.com',NULL,'2021-11-02','Hello. I am a test user. I like to suck cock!','$2a$12$SF/5hxCP//4BLg7oWzywwubl1NA1oRXEHRVPEDbkuGdvpCNBJb7Eq'),('user2','uasdf.afasd@fgmad.com',NULL,'2021-11-04',NULL,'$2a$12$XqEXCYqd2kGnoWW122swBOKGOeX3kwy6Sd8ki2r1Ig5CFGVyItrLO');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'dilog'
---
-
---
--- Dumping routines for database 'dilog'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -398,4 +389,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-03  0:26:08
+-- Dump completed on 2021-12-05 19:48:59
