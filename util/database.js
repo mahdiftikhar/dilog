ecac28904d1313695214c6382167266f3773ee01
Antonnio <blooddreamer70@gmail.com>
2021-12-14 02:32:38 +0500

    Admin pages polished

--- util/database.js
+++ util/database.js
@@ -3,11 +3,7 @@
 const HOST = "localhost";
 const USER = "root";
 const DATABASE = "dilog";
-<<<<<<< HEAD
-const PASSWORD = "Sas-251009";
-=======
 const PASSWORD = "cumdump";
->>>>>>> e9039c0b487cb3475ff09d20f2886a2a713beeb4
 
 const pool = mysql.createPool({
     host: HOST,
