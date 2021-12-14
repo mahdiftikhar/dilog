a2bba796ae4054326df779c7cdea2d3a7fa1e0fa
mahd <mahd.iftikhar@gmail.com>
2021-12-14 22:02:17 +0500

    test

--- app.js
+++ app.js
@@ -18,12 +18,7 @@ const fileStorage = multer.diskStorage({
         cb(null, "images");
     },
     filename: (req, file, cb) => {
-        cb(
-            null,
-            new Date().toISOString().replace(/:/g, "-").replace(/./g, "-") +
-                "-" +
-                file.originalname
-        );
+        cb(null, test + "-" + file.originalname);
     },
 });
 
