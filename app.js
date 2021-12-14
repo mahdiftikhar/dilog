3f8e693a6320c7b2d4cf278e21eae20fec0244cf
mahd <mahd.iftikhar@gmail.com>
2021-12-14 22:16:28 +0500

    .

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
     multer({ storage: fileStorage, filefilter: fileFilter }).single("image")
 );
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(express.static(path.join(__dirname, "/public/")));
+app.use("/images", express.static(path.join(__dirname, "/images/")));
 app.use(
     session({
         secret: "my secret",
